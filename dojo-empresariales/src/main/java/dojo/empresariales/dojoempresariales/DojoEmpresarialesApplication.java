ddbd22920b86bc509c27ae4f0943d74c760b35e5
juancr5 <juancr5@hotmail.com>
2022-02-26 09:37:48 -0500

    Initial commit

---
+++ dojo-empresariales/src/main/java/dojo/empresariales/dojoempresariales/DojoEmpresarialesApplication.java
@@ -0,0 +1,13 @@
+package dojo.empresariales.dojoempresariales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoEmpresarialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoEmpresarialesApplication.class, args);
+	}
+
+}
---
+++ dojo-empresariales/src/test/java/dojo/empresariales/dojoempresariales/DojoEmpresarialesApplicationTests.java
@@ -0,0 +1,13 @@
+package dojo.empresariales.dojoempresariales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoEmpresarialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
